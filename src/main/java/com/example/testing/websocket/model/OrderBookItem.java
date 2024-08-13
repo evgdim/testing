2c9d9ad5810642be8c4d929d7a9a3980fe079747
Evgeni Dimitrov <evgeni.dimitrov@nevexis.com>
2024-08-13 17:57:17 +0300

    add constructor for republished

--- src/main/java/com/example/testing/websocket/model/OrderBookItem.java
+++ src/main/java/com/example/testing/websocket/model/OrderBookItem.java
@@ -3,4 +3,8 @@ package com.example.testing.websocket.model;
 import java.math.BigDecimal;
 
 public record OrderBookItem(BigDecimal price, String priceStr, BigDecimal volume, String volumeStr, BigDecimal timestamp, boolean republished) {
+    public OrderBookItem(BigDecimal price, String priceStr, BigDecimal volume, String volumeStr, BigDecimal timestamp) {
+        this(price, priceStr, volume, volumeStr, timestamp, false);
+    }
+
 }
